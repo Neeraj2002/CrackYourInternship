f828d971f9181bbc5f9dfa124252895b88d6976c
Neeraj Rathod <75560703+Neeraj2002@users.noreply.github.com>
2022-06-06 20:33:10 +0530

    DAY 6

---
+++ arrays/19.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int lastGoodIndexPosition = nums.size() - 1;
+        for(int i=nums.size()-1; i>=0; i--){
+            if (i + nums[i] >= lastGoodIndexPosition){
+                lastGoodIndexPosition = i;
+            }
+        }
+        return lastGoodIndexPosition == 0;
+    }
+};
